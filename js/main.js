911d75134362b889f39cd5f2ddb4caf20110e2ea
kikimosha <eszmvcu@mail.ru>
2013-12-12 23:23:35 +0200

    add placeholders

--- js/main.js
+++ js/main.js
@@ -1,37 +1,5 @@
 $(document).ready(function(){
 
-    var formInputs = [
-        {
-            "el": '#searchText',
-            "defVal": "Поиск по сайту"
-        },
-        {
-            "el": '#reg_name',
-            "defVal": "имя"
-        },
-        {
-            "el": '#reg_password',
-            "defVal": "пароль"
-        }
-    ];
-
-    $(formInputs).each(function(i, inputObj) {
-
-        $(inputObj.el).blur(function() {
-            var currElem = this;
-            if ($(currElem).val() == "") {
-                $(currElem).val(inputObj.defVal);
-            }
-        });
-
-        $(inputObj.el).click(function() {
-            var currElem = this;
-            if ($(currElem).val() == inputObj.defVal) {
-                $(currElem).val("");
-            }
-        });
-    });
-
     $.datepicker.regional['ru'] = {
         closeText: 'Закрыть',
         prevText: '&#x3c;Пред',
