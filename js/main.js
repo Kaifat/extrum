9d83e9df2f70b5c61d55d110a98822b1d3dcc8b6
kikimosha <eszmvcu@mail.ru>
2013-12-12 23:04:38 +0200

    popup done

--- js/main.js
+++ js/main.js
@@ -32,9 +32,31 @@ $(document).ready(function(){
         });
     });
 
+    $.datepicker.regional['ru'] = {
+        closeText: 'Закрыть',
+        prevText: '&#x3c;Пред',
+        nextText: 'След&#x3e;',
+        currentText: 'Сегодня',
+        monthNames: ['Январь','Февраль','Март','Апрель','Май','Июнь',
+            'Июль','Август','Сентябрь','Октябрь','Ноябрь','Декабрь'],
+        monthNamesShort: ['Янв','Фев','Мар','Апр','Май','Июн',
+            'Июл','Авг','Сен','Окт','Ноя','Дек'],
+        dayNames: ['воскресенье','понедельник','вторник','среда','четверг','пятница','суббота'],
+        dayNamesShort: ['вск','пнд','втр','срд','чтв','птн','сбт'],
+        dayNamesMin: ['Вс','Пн','Вт','Ср','Чт','Пт','Сб'],
+        weekHeader: 'Не',
+        dateFormat: 'dd.mm.yy',
+        firstDay: 1,
+        isRTL: false,
+        showMonthAfterYear: false,
+        yearSuffix: ''};
+    $.datepicker.setDefaults($.datepicker.regional['ru']);
+
     // datepicker
     $( "#datepicker" ).datepicker({
-        inline: true
+        inline: true,
+        changeMonth: true,
+        changeYear: true
     });
 
     // registration popup
@@ -42,15 +64,11 @@ $(document).ready(function(){
         autoOpen: false,
         width: 500,
         title: "Создать кабинет",
+        modal:true,
         buttons: [
             {
-                text: "Ok",
-                click: function() {
-                    $( this ).dialog( "close" );
-                }
-            },
-            {
-                text: "Cancel",
+                text: "Зарегистрироваться",
+                "class": "orangeBg",
                 click: function() {
                     $( this ).dialog( "close" );
                 }
